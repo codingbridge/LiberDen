555f6b2a765de181d1e80b0188866c3d0da01bdc
codingbridge <frankwang.ca@gmail.com>
2019-07-30 14:25:54 -0400

    Update views.py

--- server/inventory/views.py
+++ server/inventory/views.py
@@ -9,6 +9,17 @@ class LibraryView(ListView):
     def get_queryset(self):
         return Inventory.objects.all()
 
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'inventory/upload.html', {
+        'form': form
+    })
 # def product_list_view(request):
 #     queryset = Inventory.objects.all_available()
 #     context = {
