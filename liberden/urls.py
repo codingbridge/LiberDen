77366e941ef742aa7a36db20bda803f3affae715
codingbridge <frankwang.ca@gmail.com>
2019-07-30 14:33:39 -0400

    Update urls.py

--- liberden/urls.py
+++ liberden/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import url, include
 from server.inventory.views import LibraryView
-
+from server.inventory import views
 
 urlpatterns = [
     url(r'^library/$', LibraryView.as_view(), name='Libray' ),
+    url(r'^upload/$', views.model_form_upload, name='Upload'),
     url(r'^admin/', admin.site.urls)
 ]
