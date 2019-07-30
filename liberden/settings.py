583141a0c483810960ab137fd894198e5de6a54c
codingbridge <frankwang.ca@gmail.com>
2019-07-30 14:29:18 -0400

    Update settings.py

--- liberden/settings.py
+++ liberden/settings.py
@@ -143,3 +143,6 @@ JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
     'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3600),
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
