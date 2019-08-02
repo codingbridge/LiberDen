9cfc29961e5a72bb5dead1cd5556f96020e7e547
unknown <frankwang.ca@gmail.com>
2019-08-01 23:21:42 -0400

    WIP

--- liberden/urls.py
+++ liberden/urls.py
@@ -7,9 +7,9 @@ from server.inventory import views
 from server.carts import views as cartsviews
 
 urlpatterns = [
-    url(r'^library/$', LibraryView.as_view(), name='Libray' ),
-    url(r'^upload/$', views.model_form_upload, name='Upload'),
-    url(r'^update/$', cartsviews.cart_update, name='update'),
+    url(r'^library/$', LibraryView.as_view(), name='libray' ),
+    url(r'^upload/$', views.model_form_upload, name='upload'),
+    url(r'^cart/update/$', cartsviews.cart_update, name='update'),
     # url(r'^updatecart$', views.update_cart, name='UpdateCart'),
     url(r'^admin/', admin.site.urls)
 ]
--- server/carts/views.py
+++ server/carts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import JsonResponse
+from server.inventory.models import Inventory
+from .models import Cart
+
 
 def cart_update(request):
     item_id = request.POST.get('inventory_id')
@@ -6,23 +11,29 @@ def cart_update(request):
     if item_id is not None:
         try:
             inventory_obj = Inventory.objects.get(id=item_id)
-        except Product.DoesNotExist:
-            messaage = "this item is not available."
-            return redirect("cart:home")
-        cart_obj, new_obj = Cart.objects.new_or_get(request)
-        if inventory_obj in cart_obj.item.all():
-            cart_obj.products.remove(inventory_obj)
-            added = False
+        except inventory_obj.DoesNotExist:
+            #messaage = "this item is not available."
+            return redirect("library")
+        cart_obj = Cart.objects.new_or_get(request)
+        if inventory_obj in cart_obj.Items.all():
+            cart_obj.Items.remove(inventory_obj)
+            #added = False
         else:
-            cart_obj.items.add(inventory_obj)
-            added = True
-        request.session['cart_items'] = cart_obj.items.count()
+            cart_obj.Items.add(inventory_obj)
+            #added = True
+    
+        request.session['cart_items'] = 10 #cart_obj.Items.count()
+
+
+    # if 'cart_items' in request.session:
+    #     print(request.session.cart_items, flush=True)
+    return redirect("libray")
         
-        if request.is_ajax(): # Asynchronous JavaScript And XML / JSON            
-            json_data = {
-                "added": added,
-                "removed": not added,
-                "cartItemCount": cart_obj.items.count()
-            }
-            return JsonResponse(json_data, status=200) 
-    return redirect("cart:home")
+    #     if request.is_ajax(): # Asynchronous JavaScript And XML / JSON            
+    #         json_data = {
+    #             "added": added,
+    #             "removed": not added,
+    #             "cartItemCount": cart_obj.items.count()
+    #         }
+    #         return JsonResponse(json_data, status=200) 
+    # return redirect("library")
