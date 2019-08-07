70386fb08cc5687db5ce5deed5215df528703f7d
unknown <frankwang.ca@gmail.com>
2019-08-07 19:53:03 -0400

    search function

--- server/inventory/models.py
+++ server/inventory/models.py
@@ -7,78 +7,43 @@ from server.book.models import Book, TitleSlugModel, TraceModel
 
 User = settings.AUTH_USER_MODEL
 
+def get_name_value_pair(search):
+    if ':' in search:
+        name, value = search.split(':')
+    else:
+        name = ""
+        value = search
+
+    return name, value
+
+def get_search_keywords_dict(search_string):
+    result = {}
+    search_string = search_string.lower()
+    if '|' in search_string:
+        sub_strings = search_string.split('|')
+        for sub in sub_strings:
+            name, value = get_name_value_pair(sub)
+            if name in result:
+                result[name].append(value)
+            else:
+                result[name] = [value]
+    else:
+        name, value = get_name_value_pair(search_string)
+        if name in result:
+                result[name].append(value)
+        else:
+            result[name] = [value]
+
+    return result
+
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/%Y/%m/%d/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-# class InventoryQuerySet(models.QuerySet):
-#     def availabe_copies(self):
-#         return self.filter(Q(status='A'))
-
-#     def copies_by_status(self, status_code):
-#         return self.filter(Q(status=status_code))
-    
-#     def get_by_id(self, inventoryId):
-#         return self.filter(Q(id=inventoryId))
-
-#     def book(self, bookid):
-#         return self.availabe_copies().filter(Q(book__id=bookid))
-
-#     def books(self):
-#         return self.availabe_copies().book_set.distinct()
-
-#     def books_by_category(self, name, value):
-#         return self.books().filter(Q(category__type=name) & Q(category__title=value)).distinct()
-
 class InventoryManager(models.Manager):
-    # def get_queryset(self):
-    #     return InventoryQuerySet(self.model, using=self._db)
-    
-    # def all(self):
-    #     return super().get_queryset().filter(~Q(status='R')).order_by("book")
-    
-    # def all_available(self):
-    #     return super().get_queryset().filter(Q(status='A'))
-
     def copies_count_by_id(self, bookid, status_code):
         return super().get_queryset().filter(Q(book__id=bookid) & Q(status=status_code)).count()
-
-    # def copies_count(self, status_code):
-    #     return super().get_queryset().copies_by_status(status_code).count()
-
-    # def book_copies(self, bookid):
-    #     return super().get_queryset().book(bookid)
-
-    # def book_copies_count(self, bookid):
-    #     return super().get_queryset().book(bookid).count()
-
-    # def books_count(self):
-    #     return super().get_queryset().books().count()
-    def get_name_value_pair(self, search):
-        if ':' in search:
-            name, value = search.split(':')
-        else:
-            name = None
-            value = search
-        
-        return name, value
-
-    def parse_search_string(self, search_string):
-        search = {'name':None, 'value':None}
-        search_string = search_string.lower()
-        if '|' in search_string:
-            sub_strings = search_string.split('|')
-            for sub in sub_strings:
-                name, value = get_name_value_pair(self, sub)
-                search['name'] = name
-                search['value'] = value
-                #result = self.get_filtered_queryset(result, sub)
-        else:
-            #result = self.get_filtered_queryset(result, search_string)
-
-
-        return search
 
     def get_broad_search_queryset(self):
         pass
@@ -87,79 +52,35 @@ class InventoryManager(models.Manager):
         pass   
 
     def search_by_name_value(self, queryset, name, value):
-        if name == 'a':
+        if name == 'a': #author
             result = queryset.filter(Q(book__author__title__icontains=value))
-        elif name == 'p':
+        elif name == 'p': #publisher
             result = queryset.filter(Q(book__publisher__title__icontains=value))
-        elif name == 'r':
+        elif name == 'r': #ranking
             result = queryset.filter(Q(book__ranking=value))
         elif name == 'isbn':
             result = queryset.filter(Q(book__isbn=value))
         elif name == 'status':
             result = queryset.filter(Q(status=value))
-        else:
+        elif name == '': #title, subtitle
+            result = queryset.filter(Q(book__title__icontains=value) | Q(book__sub_title__icontains=value))
+        else: #categories
             result = queryset.filter(Q(book__categories__type=name) & Q(book__categories__title=value))
         return result
 
-    def get_filtered_queryset(self, queryset, search):
-        if ':' in search:
-            name, value = search.split(':')
-        else:
-            name = None
-            value = search
-
-        if name is None or not name.strip():
-            result = queryset.filter(Q(book__title__icontains=value) | Q(book__sub_title__icontains=value))
-        elif value is None or not value.strip():
-            result = queryset.filter((Q(book__title__icontains=s) for s in name))
-        else:
-            result = self.search_by_name_value(queryset, name, value)
-
-        return result
-
     def books_search(self, search_string):
-
         result = super().get_queryset().filter(~Q(status='R')).order_by("book")
 
         if search_string is None or not search_string.strip():
             return result
 
-        search_string = search_string.lower()
-        if '|' in search_string:
-            sub_strings = search_string.split('|')
-            for sub in sub_strings:
-                result = self.get_filtered_queryset(result, sub)
-        else:
-            result = self.get_filtered_queryset(result, search_string)
-                # name, value = sub.split(':')
-                # if name is None or not name.strip():
-                #     result = result.filter((Q(book__icontains=s) for s in value))
-                # elif value is None or not value.strip():
-                #     result = result.filter((Q(book__icontains=s) for s in name))
-                # else:
-                #     if name == 'a':
-                #         result = result.filter(Q(book__author__title__icontains=value))
-                #     elif name == 'p':
-                #         result = result.filter(Q(book__publisher__title__icontains=value))
-                #     elif name == 'r':
-                #         result = result.filter(Q(book__ranking=value))
-                #     elif name == 'isbn':
-                #         result = result.filter(Q(book__isbn=value))
-                #     elif name == 'status':
-                #         result = result.filter(Q(status=value))
-                #     else:
-                #         result = result.filter(Q(book__categories__type=name) & Q(book__categories__title=value))
+        pairs = get_search_keywords_dict(search_string.lower())
+        for p in pairs:
+            for v in pairs[p]:
+                result = self.search_by_name_value(result, p, v)
 
         return result
-        
-    # def books_by_category_count(self, name, value):
-    #      return super().get_queryset().books_by_category(name, value).count()
-    
-    # def search(self, *args, **kwargs):
-    #     pass
 
-    # def is_available(self, inventoryid):
-    #     pass
 
 class Location(TitleSlugModel, TraceModel):
     pass
@@ -189,9 +110,6 @@ class Inventory(TraceModel):
     @property
     def price(self):
         return f'{self.price_currency} {self.price_amount}'
-
-    # class Meta:
-    #     order_with_respect_to = 'book'
 
 class CirculationManager(models.Manager):
     pass
